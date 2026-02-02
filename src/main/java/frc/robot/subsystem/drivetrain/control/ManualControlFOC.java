24e89d610eefe73297e2222704820f759d60a2cd
Matthew Lomando <m.lomando@outlook.com>
2026-02-02 18:47:49 -0500

    Position control updated

--- src/main/java/frc/robot/subsystem/drivetrain/control/ManualControlFOC.java
+++ src/main/java/frc/robot/subsystem/drivetrain/control/ManualControlFOC.java
@@ -3,6 +3,9 @@ package frc.robot.subsystem.drivetrain.control;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
+/**
+ * Field oriented control
+ */
 public class ManualControlFOC implements  SwerveControlIO{
     public double HorizontalVelocity;
     public double VerticalVelocity;
--- src/main/java/frc/robot/subsystem/drivetrain/control/PositionalControl.java
+++ src/main/java/frc/robot/subsystem/drivetrain/control/PositionalControl.java
@@ -1,15 +1,32 @@
 package frc.robot.subsystem.drivetrain.control;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class PositionalControl implements SwerveControlIO{
+    private final Pose2d target;
 
-    public PositionalControl(){
+    private final PIDController xController;
+    private final PIDController yController;
+    private final PIDController thetaController;
 
+    public PositionalControl(Pose2d target) {
+        xController = new PIDController(0.05, 0, 0);
+        yController = new PIDController(0.05, 0, 0);
+        thetaController = new PIDController(0.05, 0, 0);
+
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        thetaController.setTolerance(.01);
+
+        this.target = target;
     }
     @Override
-    public ChassisSpeeds getChassisSpeeds(Pose2d poseMeters) {
-        return null;
+    public ChassisSpeeds getChassisSpeeds(Pose2d currentPose) {
+        return new ChassisSpeeds(
+                xController.calculate(currentPose.getX(), target.getX()),
+                yController.calculate(currentPose.getY(), target.getY()),
+                thetaController.calculate(currentPose.getRotation().getRadians())
+        );
     }
 }
